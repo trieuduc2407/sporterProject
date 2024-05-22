8c5a2eecc3dba2bc8467ecc0f7d1781f13f048ab
trieuduc2407 <trieuduc2407@gmail.com>
2024-05-22 23:28:54 +0700

    add main db, login and logout

--- test/main.py
+++ test/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import sqlite3
-import requests
 
 app = Flask(__name__)
 sqldbname = 'database.db'
 app.secret_key = 'test'
+
 
 # Dinh tuyen ham index cho url mac dinh
 @app.route('/', methods=['get'])
@@ -12,17 +12,19 @@ def index():
     # Render file index.html khi trinh duyet tro den url '/' (url mac dinh)
     return render_template('index.html')
 
+
 # Dinh tuyen ham team cho url '/team/ten doi bong' vd: '/team/manu'
-@app.route('/team/<team>', methods=['get'])
-def team(team):
+@app.route('/team/<fteam>', methods=['get'])
+def team(fteam):
     conn = sqlite3.connect(sqldbname)
     c = conn.cursor()
-    c.execute('SELECT * FROM products WHERE team = ?', (team,))
+    c.execute('SELECT * FROM products WHERE team = ?', (fteam,))
     # Tim kiem cac san pham la ao dau cua manu va luu vao products
     products = c.fetchall()
     conn.close()
     # Render file team.html va truyen vao gia tri cua bien products
     return render_template('team.html', products=products)
+
 
 # Dinh tuyen ham nation cho url '/nations'
 @app.route('/nations', methods=['get'])
@@ -36,8 +38,39 @@ def nations():
     # Render file nation.html va truyen vao gia tri cua bien products
     return render_template('nation.html', products=products)
 
-# Dinh tuyen ham login cho url '/login'
 
+# Dinh tuyen ham login cho url '/login'
+@app.route('/login', methods=['get', 'post'])
+def login():
+    # Neu method la post thi lay data tu form
+    if request.method == 'post':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Tao bien check
+        conn = sqlite3.connect(sqldbname)
+        c = conn.cursor()
+        # Tim kiem user va password trong table users
+        c.execute('SELECT * FROM "users " WHERE username = ? AND password = ?', (username, password))
+        # Luu ket qua tim kiem dau tien vao bien user
+        user = c.fetchone()
+        conn.close()
+        if user:
+            session['logged_in'] = True
+            session['username'] = user[1]
+            session['id'] = user[0]
+            return render_template(url_for('index'))
+    elif request.method == 'get':
+        return render_template('login.html')
+
+
+# Dinh tuyen ham logout cho url '/logout'
+@app.route('/logout')
+def logout():
+    # Xoa session username va chuyen huong den trang index
+    session.pop('username', None)
+    session.pop('logged_in', None)
+    session.pop('id', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
