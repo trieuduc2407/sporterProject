d09a33899545c83c33c94615e6ecf8b2f0cdca1a
trieuduc2407 <trieuduc2407@gmail.com>
2024-07-05 16:21:07 +0700

    16h20-5/7/24: Add new table

--- test/static/js/carousel.js
+++ test/static/js/carousel.js
@@ -2,7 +2,6 @@ function formatString(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ".") + " " + "VND";
 }
 
-
 let price = document.querySelectorAll('.item-price')
 
 for (let i = 0; i < price.length; i++) {
